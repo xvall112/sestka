dc710a89597a551ecaede474ee1a81714ad77b52
Lukas Valta <valtalukas@seznam.cz>
2021-02-16 13:36:45 +0100

    cena

--- src/template/RdHouse/components/index.js
+++ src/template/RdHouse/components/index.js
@@ -1,4 +1,4 @@
 export { default as Info } from './Info';
 export { default as Hero } from './Hero';
-export { default as SwiperImage } from './SwiperImage';
+
 export { default as CardJobMinimalPrice } from './CardJobMinimalPrice';
